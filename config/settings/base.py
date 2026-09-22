b2cd0db089a0facd3f44ec8a7d66fecbb5f5db61
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-23 00:50:36 +0545

    user register

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",
     "django_filters",
     "drf_spectacular",
+    "storages"
 ]
 
 LOCAL_APPS: list[str] = ["apps.user"]
--- config/settings/development.py
+++ config/settings/development.py
@@ -13,3 +13,8 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ],
 }
+# File storage for s3
+DEFAULT_FILE_STORAGES="storages.backends.s3boto3.S3Boto3Storage"
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="ap-south-1")
+MEDIA_URL = f"https://{AWS_STORAGE_BU`CKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
