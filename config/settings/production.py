e70663c59d28e581f6ec5d34d3a5b68e8ffe44a2
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-23 01:11:16 +0545

    user register

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,7 +19,7 @@ CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # File storage for s3
-DEFAULT_FILE_STORAGES="storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="ap-south-1")
 MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
