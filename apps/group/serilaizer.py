91ef158389f281d5081e23b71e74e7130f342d1b
ram2005024 <sharmashekhar20050@gmail.com>
2026-09-26 23:55:36 +0545

    group view create logic override

--- apps/group/serilaizer.py
+++ apps/group/serilaizer.py
@@ -7,31 +7,26 @@ from apps.group.models import GroupMembership, Group
 
 User=get_user_model()
 
+
+
 class GroupMemberSerializer(serializers.ModelSerializer):
-    full_name=serializers.CharField(source="profile.full_name",read_only=True)
-    image=serializers.ImageField(source="profile.image",read_only=True,allow_null=True,allow_empty_file=True)
-    class Meta:
-        model=User
-        fields=[
-            "id",
-            "email",
-            "full_name",
-            "image"
-        ]
+    id=serializers.UUIDField(source="user.id")
+    email=serializers.EmailField(source="user.email")
+    full_name=serializers.CharField(source="user.profile.full_name")
+    image=serializers.CharField(source="user.profile.image")
 
-
-
-class GroupMembershipSerializer(serializers.ModelSerializer):
-    user=GroupMemberSerializer(read_only=True)
     class Meta:
         model=GroupMembership
-        fields=["user"]
+        fields=["id",
+            "email",
+            "full_name",
+            "image"]
 
 
 
 
 class GroupSerializer(serializers.ModelSerializer):
-    memberships=GroupMembershipSerializer(many=True)
+    memberships=GroupMemberSerializer(many=True,read_only=True)
     user_ids=serializers.ListField(default=[],child=serializers.UUIDField(),required=False,write_only=True)
     class Meta:
         model=Group
@@ -48,8 +43,9 @@ class GroupSerializer(serializers.ModelSerializer):
         ]
     @transaction.atomic
     def create(self, validated_data):
-        membership_ids=validated_data.pop("user_ids",None)
-        creator=validated_data.get("created_by",None)
+        membership_ids=validated_data.pop("user_ids",[])
+        request=self.context.get("request")
+        creator=getattr(request,"user",None)
         group=Group.objects.create(**validated_data)
         if creator:
             membership_ids.append(creator.id)
